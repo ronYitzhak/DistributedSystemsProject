5363c9094b36c52000a4cacda57d5bc84c48643f
GalSha <28822162+GalSha@users.noreply.github.com>
2019-12-22 12:02:49 +0200

    voteClientTest

---
+++ src/main/java/VoteClientTest.java
@@ -0,0 +1,40 @@
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.apache.zookeeper.server.quorum.Vote;
+import protos.VoterGrpc;
+import protos.VoterOuterClass;
+
+import java.util.Iterator;
+
+public class VoteClientTest {
+    private VoterGrpc.VoterBlockingStub stub;
+    private ManagedChannel channel;
+
+    public VoteClientTest(String host, int port) {
+        channel = ManagedChannelBuilder
+                .forAddress(host, port)
+                .usePlaintext()
+                .build();
+        stub = VoterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() {
+        channel.shutdown();
+    }
+
+    public void vote(String voterName, int candidateId, int state) {
+        VoterOuterClass.VoteRequest v = VoterOuterClass.VoteRequest.newBuilder()
+                .setVoterName(voterName)
+                .setCandidateId(candidateId)
+                .setState(state)
+                .build();
+        stub.vote(v);
+    }
+
+    public static void main(String[] args) throws Exception {
+        VoteClientTest client = new VoteClientTest("localhost", 50051);
+        client.vote("gal", 0, 1);
+        client.shutdown();
+    }
+
+}
