cf468c645fb82ae2ad628dd9b343f78951552b09
Ron Yitzhak <rony@microsoft.com>
2019-12-26 20:09:34 +0200

    remove rest server class (redundant)

--- src/main/java/Application/Election.java
+++ src/main/java/Application/Election.java
@@ -2,7 +2,10 @@ package Application;
 
 import Impl.VoteImpl;
 import Impl.ZooKeeperService;
+import RestClient.Controllers.VotesController;
 import org.apache.zookeeper.KeeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -12,8 +15,7 @@ import java.util.Scanner;
 
 @SpringBootApplication
 public class Election {
-    // TODO: init all servers, zk, rest, etc.
-    // main function here
+    private static final Logger LOG = LoggerFactory.getLogger(VoteImpl.class);
 
     public static void main(String[] args) {
 //        org.apache.log4j.BasicConfigurator.configure();
@@ -23,9 +25,11 @@ public class Election {
         HashMap<String, Object> props = new HashMap<>();
         props.put("server.port", restPort);
         new SpringApplicationBuilder()
-                .sources(Election.class)
+                .sources(VotesController.class)
                 .properties(props)
                 .run();
+
+        LOG.info("rest initialized on port " + restPort);
 
         Scanner input = new Scanner(System.in);
         System.out.print("gRPC self ip: ");
@@ -34,21 +38,24 @@ public class Election {
         int grpcPort = input.nextInt();
         String zkHost = "127.0.0.1:2181";
 
-        VoteImpl voteServer = null; // zkHost: "127.0.0.1:2181"
         try {
-            voteServer = new VoteImpl(host+":"+grpcPort, "California", grpcPort);
+            VoteImpl voteServer = new VoteImpl(host+":"+grpcPort, "California", grpcPort);
+            LOG.info("VoteImpl initialized on host: " + host + " and port: " + grpcPort);
             ZooKeeperService.init(zkHost, voteServer);
+            LOG.info("ZooKeeperService initialized on host: " + zkHost);
             voteServer.propose();
+            LOG.info("VoteImpl proposed on host: " + host + " and port: " + grpcPort);
+            System.out.println("Hello");
+            while (true) {}
         } catch (IOException e) {
+            LOG.info("IOException - should not get here");
             e.printStackTrace();
         } catch (InterruptedException e) {
+            LOG.info("InterruptedException - should not get here");
             e.printStackTrace();
         } catch (KeeperException e) {
+            LOG.info("KeeperException - should not get here");
             e.printStackTrace();
         }
-
-        System.out.println("Hello");
-
-        while (true) {}
     }
 }
